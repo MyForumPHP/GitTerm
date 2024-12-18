076ccf5c263e759674d19a92b9768913db47f731
MyForumPHP <mogd4k@gmail.com>
2024-12-18 16:31:24 +0800

    Add files via upload

---
+++ Start.py
@@ -0,0 +1,9 @@
+print("by mogd")
+verb = input("1-установка")
+verb = input("2-запуск")
+if verb == "1":
+    print("chmod 777 install-nethunter-termux")
+elif verb == "2":
+print("./install-nethunter-termux")
+else:
+    print("что-то я тебя не понял")
